4d9e952076d8f66f928678fe79090f352a432476
rafi <rafibreuer@gmail.com>
2017-08-17 14:16:01 -0400

    08/17

--- warehouse/repo/index.js
+++
@@ -1,8 +0,0 @@
-const auth = require('./basicAuth');
-const bin = require('./bin');
-const user = require('./user');
-
-
-module.exports=auth;
-module.exports=bin;
-module.exports=user;
--- warehouse/repo/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    userName: { name: String, password: String },
-    perm: String
-}, { collections: 'users' });
-
-module.exports = mongoose.model('user', userSchema);
